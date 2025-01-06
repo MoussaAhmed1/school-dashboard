599d0d911b43ec62ce4d1231853398f0d732ab32
Mousa Ahmed <moussa.abdelghany@gmail.com>
2025-01-06 14:26:30 +0200

    Update static-pages.ts

--- actions/settings/static-pages.ts
+++ actions/settings/static-pages.ts
@@ -8,11 +8,13 @@ import axiosInstance, { endpoints, getErrorMessage } from "@/utils/axios-client"
 
 export const fetchStaticPages = async (page:"PRIVACY_POLICY" | "TERMS_AND_CONDITIONS"|"ABOUT_US"): Promise<any> => {
   const accessToken = cookies().get("access_token")?.value;
+  const lang = cookies().get("Language")?.value;
 
   try {
     const res = await axiosInstance(endpoints.generalSettings.root + "/" + page, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
+        "Accept-Language": lang,
       },
     });
     return res.data.data;
